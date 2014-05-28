56dba605809d6d06409ac0dedd3b2d7ab42c198d
Jean-Francois Nadeau <jfnadeau@inboxroute.com>
2014-05-28 15:05:46 -0400

    added missing Object in data

--- event.go
+++ event.go
@@ -1,14 +1,18 @@
 package stripe
 
 type Event struct {
-	Id              string                 `json:"id"`
-	Livemode        bool                   `json:"livemode"`
-	Created         int64                  `json:"created"`
-	Data            map[string]interface{} `json:"data"`
-	PendingWebhooks int                    `json:"pending_webhooks"`
-	Type            string                 `json:"type"`
-	Request         string                 `json:"request"`
-	UserId          string                 `json:"user_id,omitempty"` // Stripe connect
+	Id              string    `json:"id"`
+	Livemode        bool      `json:"livemode"`
+	Created         int64     `json:"created"`
+	Data            EventData `json:"data"`
+	PendingWebhooks int       `json:"pending_webhooks"`
+	Type            string    `json:"type"`
+	Request         string    `json:"request"`
+	UserId          string    `json:"user_id,omitempty"` // Stripe connect
+}
+
+type EventData struct {
+	Object map[string]interface{} `json:"object"`
 }
 
 const (

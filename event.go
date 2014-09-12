0ef3529a8402d6fec2abd0d4894e36d076de5ec4
Jean-Francois Nadeau <jfnadeau@inboxroute.com>
2014-09-12 15:51:10 -0400

    Add event types

--- event.go
+++ event.go
@@ -12,11 +12,19 @@ type Event struct {
 }
 
 type EventData struct {
-	Object map[string]interface{} `json:"object"`
+	Object             map[string]interface{} `json:"object"`
+	PreviousAttributes map[string]interface{} `json:"previous_attributes"`
 }
 
 const (
-	EventType_AccountUpdated      = "account.updated"
-	EventType_InvoiceCreated      = "invoice.created"
-	EventType_SubscriptionDeleted = "customer.subscription.deleted"
+	EventType_AccountUpdated          = "account.updated"
+	EventType_InvoiceCreated          = "invoice.created"
+	EventType_SubscriptionDeleted     = "customer.subscription.deleted"
+	EventType_CustomerDiscountCreated = "customer.discount.created"
+	EventType_CustomerDiscountUpdated = "customer.discount.updated"
+	EventType_CustomerDiscountDeleted = "customer.discount.deleted"
+	EventType_CustomerDeleted         = "customer.deleted"
+	EventType_CustomerUpdated         = "customer.updated"
+	EventType_SubscriptionUpdated     = "customer.subscription.updated"
+	EventType_InvoiceItemCreated      = "invoiceitem.created"
 )
